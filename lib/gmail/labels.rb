1fbb3ea9bed96c922ffe25ef534f52fd17db8147
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2014-03-20 11:46:35 +0900

    nested label support

--- lib/gmail/labels.rb
+++ lib/gmail/labels.rb
@@ -3,44 +3,58 @@ module Gmail
     include Enumerable
     attr_reader :connection
     alias :conn :connection
-     
+
     def initialize(connection)
       @connection = connection
     end
-    
+
     # Get list of all defined labels.
     def all
-      (conn.list("", "%")+conn.list("[Gmail]/", "%")).inject([]) do |labels,label|
+      @list = []
+
+      ## check each item in list for subfolders
+      conn.list("", "%").each {|l| sublabels_or_label(l)}
+
+      @list.inject([]) do |labels,label|
         label[:name].each_line {|l| labels << Net::IMAP.decode_utf7(l) }
-        labels 
+        labels
       end
     end
     alias :list :all
     alias :to_a :all
 
+    def sublabels_or_label(label)
+      if label.attr.include? :Hasnochildren
+        @list << label
+      else
+        @list << label
+        conn.list("#{label.name}/", "%").each {|l| sublabels_or_label(l)}
+      end
+    end
+
     def each(*args, &block)
       all.each(*args, &block)
     end
-    
-    # Returns +true+ when given label defined. 
+
+    # Returns +true+ when given label defined.
     def exists?(label)
       all.include?(Net::IMAP.encode_utf7(label))
     end
     alias :exist? :exists?
-    
+
     # Creates given label in your account.
     def create(label)
       !!conn.create(Net::IMAP.encode_utf7(label)) rescue false
     end
     alias :new :create
     alias :add :create
-    
-    # Deletes given label from your account. 
+
+    # Deletes given label from your account.
     def delete(label)
       !!conn.delete(Net::IMAP.encode_utf7(label)) rescue false
     end
     alias :remove :delete
-    
+
     def inspect
       "#<Gmail::Labels#{'0x%04x' % (object_id << 1)}>"
     end
